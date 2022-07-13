69924c38f75a0ebaf0719d2d7646a646491ec5ad
gkoum-7 <80713015+gkoum-7@users.noreply.github.com>
2022-07-13 14:27:00 -0400

    all - very descriptive see deploy.js

---
+++ hardhat.config.js
@@ -0,0 +1,14 @@
+require("@nomiclabs/hardhat-ethers");
+require("@nomiclabs/hardhat-waffle");
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.9",
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      chainId: 31337,
+      allowUnlimitedContractSize: true
+    }
+  }
+};
